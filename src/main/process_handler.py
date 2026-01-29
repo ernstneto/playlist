befc8dacb7852cf47ed156f468e398a88a06fc50
Ernst <ernstneto94@hotmail.com>
2026-01-29 13:39:43 -0300

    feat: Implementing song_register_view and constructor with song_register_constructor

--- src/main/process_handler.py
+++ src/main/process_handler.py
@@ -1,17 +1,10 @@
 from src.view.first_view import introduction_page
+from .constructor.song_register_constructor import song_register_process
 
 def start() -> None:
   while True:
-    #introduction_page()
-    """
-    command = input("Opção desejado: ")
-    if command == '1': print("Inserindo música")
-    elif command == '2': print("Criando Playlist")
-    elif command == '5': exit()
-    else: print("\nComando inválido\n\n")
-    """
     command = introduction_page()
-    if command == '1': print("\nInserindo música\n")
+    if command == '1': song_register_process()
     elif command == '2': print("\nCriando Playlist\n")
     elif command == '5': exit()
     else: print("\nComando inválido\n\n")
