02587201a1eb635885722dd18773c43378a76ef3
Ernst <ernstneto94@hotmail.com>
2026-01-30 13:29:20 -0300

    feat: Implementing song_register_controller

---
+++ src/controller/song_register_controller.py
@@ -0,0 +1,40 @@
+class SongRegisterController:
+  def insert(self, new_song_informations: dict) -> dict:
+    try:  
+      self.__verify_songs_infos()
+      self.__verify_if_song_already_registered(new_song_informations)
+      self.__insert_song(new_song_informations)
+      return self.__format_response(new_song_informations)
+    except Exception as e:
+      self.__format__error_resposne(e)
+  
+  def __verify_if_song_already_registered(new_song_informations: dict) -> None:
+    pass
+
+  def __verify_songs_infos(new_song_informations: dict) -> None:
+    if(len(new_song_informations['title'])> 100):
+      raise ValueError("O título da música não pode exceder 100 caracteres.")
+    
+    year = int(new_song_informations['year'])
+    if(year > 2025):
+      raise ValueError("O ano de publicação deve ser até 2025.")
+    
+  def __insert_song() -> None:
+    pass
+
+  def __format_response(self, new_song_informations: dict) -> dict:
+    return {
+      "success": True,
+      "count": 1,
+      "attributes": {
+        "title": new_song_informations['title'],
+        "artist": new_song_informations['artist'],
+        "year": new_song_informations['year']
+      }
+    }
+  
+  def __format__error_resposne(self, err: Exception) -> dict:
+    return {
+      "success": False,
+      "error": str(err)
+    }
